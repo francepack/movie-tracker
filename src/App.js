58009effc7a6e250e4e95b6ebfd69a6854e391ea
Isaac Sunoo <isaacsunoo@gmail.com>
2019-03-20 14:10:09 -0600

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import { NavLink, Route } from 'react-router-dom';
+import { NavLink, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Movies from './components/Movies/Movies';
 
@@ -7,8 +7,14 @@ class App extends Component {
   render() {
     return (
       <div className='app'>
-        <Header />
-        <Movies />
+        <div>
+          <Header />
+          <Movies />
+        </div>
+        <Route exact path='/' component={Movies} />
+        {/* <Route exact path='/movie/:id' render={({ match }) => {
+          console.log(match)
+        }) */}
       </div>
     );
   }
