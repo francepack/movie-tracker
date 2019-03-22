ab6251eb52af15a3ecd30d474968493c13ac09ec
Isaac Sunoo <isaacsunoo@gmail.com>
2019-03-22 10:49:59 -0600

    sign up fully funtional Co-authored-by: Mason France <masonrf1@gmail.com>

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -19,7 +19,7 @@ class Login extends Component {
     })
   }
 
-  settings = (method, body) => ({
+  options = (method, body) => ({
     method,
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(body)
@@ -28,14 +28,13 @@ class Login extends Component {
   postFetch = async (url, method, body) => {
     const backEndUrl = 'http://localhost:3000/api/';
     try {
-      const response = await fetch(`${backEndUrl}${url}`, this.settings(method, body))
+      const response = await fetch(`${backEndUrl}${url}`, this.options(method, body))
       console.log('response: ', response)
       const user = await response.json()
       console.log('user data', user.data)
       return user.data;
     } catch (err) {
       console.log('err: ', err)
-
       // throw new Error(err);
     }
   }
