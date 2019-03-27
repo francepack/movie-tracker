107bec90e7d3d18a9004992f12cd06d9ecf13674
Mason France <masonrf1@gmail.com>
2019-03-26 19:44:17 -0600

    Add tests for reducers

--- src/reducers/favorite.js
+++ src/reducers/favorite.js
@@ -3,11 +3,8 @@ export const favorite = (state = [], action) => {
   switch (action.type) {
     case 'TOGGLE_FAVORITE':
     if (state.includes(id)){
-      console.log('reducers id', id)
-        console.log('togglefav if entered')
         return state.filter(idNum => idNum !== id)
       } else {
-        console.log('togglefav else entered')
         return [...state, id];
       }
     default:
