5e3780ffe37025a9b98f5acf7e997574359eda29
Mason France <masonrf1@gmail.com>
2019-03-26 21:11:02 -0600

    Add tests for SignUpForm

--- src/components/SignUpForm/SignUpForm.test.js
+++ src/components/SignUpForm/SignUpForm.test.js
@@ -23,11 +23,40 @@ describe('SignUpForm', () => {
     });
   });
 
-  it('should handleChange', () => {
+  describe('handleChange', () => {
+    it('should update state name according to what is typed', () => {
+      const mockEvent = {target: {name:'name', value:'Isaac'}};
+      wrapper.instance().handleChange(mockEvent);
+      expect(wrapper.state('name')).toEqual('Isaac');
+    });
+    it('should update state email according to what is typed', () => {
+      const mockEvent = {target: {name:'email', value:'eyesack@aol.com'}};
+      wrapper.instance().handleChange(mockEvent);
+      expect(wrapper.state('email')).toEqual('eyesack@aol.com');
+    });
+    it('should update state password according to what is typed', () => {
+      const mockEvent = {target: {name:'password', value:'ILoveLamp'}};
+      wrapper.instance().handleChange(mockEvent);
+      expect(wrapper.state('password')).toEqual('ILoveLamp');
+    });
 
   });
 
-  it('should handleSignUp', () => {
 
+  describe('handleSignUp', () => {
+    it('should call handleSignup when form is submitted', () => {
+      const spy = spyOn(wrapper.instance(), 'handleSignUp');
+      const mockEvent = {preventDefault: jest.fn()};
+      wrapper.instance().forceUpdate();
+      wrapper.find('form').simulate('submit', mockEvent);
+      expect(spy).toHaveBeenCalled();
+    });
+    it('should', () => {
+      const mockEvent = {preventDefault: jest.fn()};
+
+    });
+    it('', () => {
+
+    });
   });
 });
